414fdfc5edfd1648b624136fef6755f9d9167ce9
Ichirinko <1621543655@qq.com>
2023-07-14 18:33:30 +0800

    add:添加了用户评价页

---
+++ src/components/EvaluatePage.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="evaluate-page">
+
+</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.evaluate-page {
+    background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAACFJREFUeNpiZGBg4GegAsBlCD8TqSYNQg2Mo6FEBAAIMACdPABtrSW/IQAAAABJRU5ErkJggg==") repeat fixed;
+    -webkit-font-smoothing: antialiased;
+    text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.04);
+    min-height: 100%;
+    margin: 0;
+}
+</style>
